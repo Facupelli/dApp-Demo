cdba92fa7a86fdfe88503e21f41e916e03cc1d9f
Facupelli <facundopellicer4@gmail.com>
2022-01-20 15:03:31 -0300

    smart contract

---
+++ client/config-overrides.js
@@ -0,0 +1,8 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+
+module.exports = function override(config, env) {
+    // New config, e.g. config.plugins.push...
+    config.plugins.push(new NodePolyfillPlugin())
+    return config
+}
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,39 @@
+import { STOREYOURPET_ADDRESS, STOREYOURPET_ABI } from './config.js';
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
 
 function App() {
+  const [account, setAccount] = useState();
+  const [contactList, setContactList] = useState();
+  const [contacts, setContacts] = useState([]);
+
+  console.log('ACCOUNT', account)
+  console.log('PETLIST',contactList)
+  console.log('PETS',contacts)
+
+  useEffect(() => {
+    async function load() {
+      const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
+      const accounts = await web3.eth.requestAccounts();
+      setAccount(accounts[0]);
+      // Instantiate smart contract using ABI and address.
+      const contactList = new web3.eth.Contract(STOREYOURPET_ABI, STOREYOURPET_ADDRESS);
+      // set contact list to state variable.
+      setContactList(contactList);
+      // Then we get total number of contacts for iteration
+      const counter = await contactList.methods.ownerPetCount('0x54e58C3daB9897df9fb11feF56e03e10B538b2DC').call();
+      // iterate through the amount of time of counter
+      console.log('COUNTER',counter)
+      for (var i = 1; i <= counter; i++) {
+        // call the contacts method to get that particular contact from smart contract
+        const contact = await contactList.methods.contacts(i).call();
+        // add recently fetched contact to state variable.
+        setContacts((contacts) => [...contacts, contact]);
+      }
+    }
+    load();
+  }, []);
+
   return (
     <div className="App">
       <p>DAPP DEMO  </p>
